d99e8b322b27fd2edc365aa096717cd9d0a63210
Esther Shue <EstherFShue@gmail.com>
2023-04-08 12:27:04 -0400

    Update source.py
    yes

--- source.py
+++ source.py
@@ -1,7 +1,5 @@
-dinerName
-amountOfPeople
 
 def get_busyness_percentage(maximumCapacity, amountOfPeople):
-    return percentage = (amountOfPeople / maximumCapacity) * 100
+    return (amountOfPeople / maximumCapacity) * 100
         
 
