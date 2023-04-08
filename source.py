5ac6a1801456d348448e533a5e89d27734d8ec6f
Esther Shue <EstherFShue@gmail.com>
2023-04-08 12:52:55 -0400

    hh
    gh

--- source.py
+++ source.py
@@ -1,6 +1,6 @@
 
 def get_busyness_percentage(maximumCapacity, amountOfPeople):
-    return int(float(amountOfPeople) / float(maximumCapacity) * 100)
+    return str(int(float(amountOfPeople) / float(maximumCapacity) * 100))
 
 
         
