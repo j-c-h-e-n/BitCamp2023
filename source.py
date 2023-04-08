f2d26da0b031857e914e825a0467463cd9750306
Esther Shue <EstherFShue@gmail.com>
2023-04-08 13:53:13 -0400

    rrr
    rrrrr

--- source.py
+++ source.py
@@ -1,7 +1,233 @@
+from datetime import datetime
 
-def get_busyness_percentage(maximumCapacity, amountOfPeople):
+now = datetime.now()
+
+def get_busyness_percentage(nameOfDiner, maximumCapacity):
+    listaa = get_time_info()
+
+    #Sunday 10am - 9pm
+    if listaa[1] == 6:
+        if listaa[0] == 10:
+            amountOfPeople = 10
+        elif listaa[0] == 11:
+            amountOfPeople = 10
+        elif listaa[0] == 12:
+            amountOfPeople = 10
+        elif listaa[0] == 13:
+            amountOfPeople = 10
+        elif listaa[0] == 14:
+            amountOfPeople = 10
+        elif listaa[0] == 15:
+            amountOfPeople = 10
+        elif listaa[0] == 16:
+            amountOfPeople = 10
+        elif listaa[0] == 17:
+            amountOfPeople = 10
+        elif listaa[0] == 18:
+            amountOfPeople = 10
+        elif listaa[0] == 19:
+            amountOfPeople = 10
+        elif listaa[0] == 20:
+            amountOfPeople = 10
+        elif listaa[0] == 21:
+            amountOfPeople = 10
+
+    #Monday 7am - 9pm
+    elif listaa[1] == 0:
+        if listaa[0] == 7:
+            amountOfPeople = 10
+        elif listaa[0] == 8:
+            amountOfPeople = 10
+        elif listaa[0] == 9:
+            amountOfPeople = 10
+        elif listaa[0] == 10:
+            amountOfPeople = 10
+        elif listaa[0] == 11:
+            amountOfPeople = 10
+        elif listaa[0] == 12:
+            amountOfPeople = 10
+        elif listaa[0] == 13:
+            amountOfPeople = 10
+        elif listaa[0] == 14:
+            amountOfPeople = 10
+        elif listaa[0] == 15:
+            amountOfPeople = 10
+        elif listaa[0] == 16:
+            amountOfPeople = 10
+        elif listaa[0] == 17:
+            amountOfPeople = 10
+        elif listaa[0] == 18:
+            amountOfPeople = 10
+        elif listaa[0] == 19:
+            amountOfPeople = 10
+        elif listaa[0] == 20:
+            amountOfPeople = 10
+        elif listaa[0] == 21:
+            amountOfPeople = 10
+
+    #Tuesday 7am - 9pm
+    elif listaa[1] == 1:
+        if listaa[0] == 7:
+            amountOfPeople = 10
+        elif listaa[0] == 8:
+            amountOfPeople = 10
+        elif listaa[0] == 9:
+            amountOfPeople = 10
+        elif listaa[0] == 10:
+            amountOfPeople = 10
+        elif listaa[0] == 11:
+            amountOfPeople = 10
+        elif listaa[0] == 12:
+            amountOfPeople = 10
+        elif listaa[0] == 13:
+            amountOfPeople = 10
+        elif listaa[0] == 14:
+            amountOfPeople = 10
+        elif listaa[0] == 15:
+            amountOfPeople = 10
+        elif listaa[0] == 16:
+            amountOfPeople = 10
+        elif listaa[0] == 17:
+            amountOfPeople = 10
+        elif listaa[0] == 18:
+            amountOfPeople = 10
+        elif listaa[0] == 19:
+            amountOfPeople = 10
+        elif listaa[0] == 20:
+            amountOfPeople = 10
+        elif listaa[0] == 21:
+            amountOfPeople = 10
+
+    #Wednesday 7am - 9pm
+    elif listaa[1] == 2:
+        if listaa[0] == 7:
+            amountOfPeople = 10
+        elif listaa[0] == 8:
+            amountOfPeople = 10
+        elif listaa[0] == 9:
+            amountOfPeople = 10
+        elif listaa[0] == 10:
+            amountOfPeople = 10
+        elif listaa[0] == 11:
+            amountOfPeople = 10
+        elif listaa[0] == 12:
+            amountOfPeople = 10
+        elif listaa[0] == 13:
+            amountOfPeople = 10
+        elif listaa[0] == 14:
+            amountOfPeople = 10
+        elif listaa[0] == 15:
+            amountOfPeople = 10
+        elif listaa[0] == 16:
+            amountOfPeople = 10
+        elif listaa[0] == 17:
+            amountOfPeople = 10
+        elif listaa[0] == 18:
+            amountOfPeople = 10
+        elif listaa[0] == 19:
+            amountOfPeople = 10
+        elif listaa[0] == 20:
+            amountOfPeople = 10
+        elif listaa[0] == 21:
+            amountOfPeople = 10
+
+    #Thrusday 7am - 9pm
+    elif listaa[1] == 3:
+        if listaa[0] == 7:
+            amountOfPeople = 10
+        elif listaa[0] == 8:
+            amountOfPeople = 10
+        elif listaa[0] == 9:
+            amountOfPeople = 10
+        elif listaa[0] == 10:
+            amountOfPeople = 10
+        elif listaa[0] == 11:
+            amountOfPeople = 10
+        elif listaa[0] == 12:
+            amountOfPeople = 10
+        elif listaa[0] == 13:
+            amountOfPeople = 10
+        elif listaa[0] == 14:
+            amountOfPeople = 10
+        elif listaa[0] == 15:
+            amountOfPeople = 10
+        elif listaa[0] == 16:
+            amountOfPeople = 10
+        elif listaa[0] == 17:
+            amountOfPeople = 10
+        elif listaa[0] == 18:
+            amountOfPeople = 10
+        elif listaa[0] == 19:
+            amountOfPeople = 10
+        elif listaa[0] == 20:
+            amountOfPeople = 10
+        elif listaa[0] == 21:
+            amountOfPeople = 10
+
+    #Friday 7am - 9pm
+    elif listaa[1] == 4:
+        if listaa[0] == 7:
+            amountOfPeople = 10
+        elif listaa[0] == 8:
+            amountOfPeople = 10
+        elif listaa[0] == 9:
+            amountOfPeople = 10
+        elif listaa[0] == 10:
+            amountOfPeople = 10
+        elif listaa[0] == 11:
+            amountOfPeople = 10
+        elif listaa[0] == 12:
+            amountOfPeople = 10
+        elif listaa[0] == 13:
+            amountOfPeople = 10
+        elif listaa[0] == 14:
+            amountOfPeople = 10
+        elif listaa[0] == 15:
+            amountOfPeople = 10
+        elif listaa[0] == 16:
+            amountOfPeople = 10
+        elif listaa[0] == 17:
+            amountOfPeople = 10
+        elif listaa[0] == 18:
+            amountOfPeople = 10
+        elif listaa[0] == 19:
+            amountOfPeople = 10
+        elif listaa[0] == 20:
+            amountOfPeople = 10
+        elif listaa[0] == 21:
+            amountOfPeople = 10
+
+    #Saturday 10am - 9pm
+    elif listaa[1] == 5:
+        if listaa[0] == 10:
+            amountOfPeople = 10
+        elif listaa[0] == 11:
+            amountOfPeople = 10
+        elif listaa[0] == 12:
+            amountOfPeople = 10
+        elif listaa[0] == 13:
+            amountOfPeople = 10
+        elif listaa[0] == 14:
+            amountOfPeople = 10
+        elif listaa[0] == 15:
+            amountOfPeople = 10
+        elif listaa[0] == 16:
+            amountOfPeople = 10
+        elif listaa[0] == 17:
+            amountOfPeople = 10
+        elif listaa[0] == 18:
+            amountOfPeople = 10
+        elif listaa[0] == 19:
+            amountOfPeople = 10
+        elif listaa[0] == 20:
+            amountOfPeople = 10
+        elif listaa[0] == 21:
+            amountOfPeople = 10
+
     return str(int(float(amountOfPeople) / float(maximumCapacity) * 100))
 
-
-        
-
+def get_time_info():
+    current_hour = now.strftime("%H")
+    current_weekday = now.today().weekday()
+    timeList = [current_hour, current_weekday]
+    return timeList
